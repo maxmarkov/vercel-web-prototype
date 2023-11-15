ebad0a82a074a9515389ceb731f9514f5debb576
maxmarkov <maxmarkov@gmail.com>
2023-11-15 23:40:03 +0100

    Footer template icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,44 @@
+import React, { useState } from 'react';
 import './Footer.css'; 
-import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
-    return (
-      <footer className='footer'>
-        {/* Footer content here */}
+  const [emoji, setEmoji] = useState('😊');
+
+  const handleMouseEnter = () => setEmoji('😄');
+  const handleMouseLeave = () => setEmoji('😊');
+
+  return (
+      <footer className="footer">
+          <div className="footer-section left">
+              <span>&copy; 2023 Your Company Name</span>
+          </div>
+          <div className="footer-section center">
+              <a href="/" className="emoji-link" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                  <div className="smiling-emoji">{emoji}</div>
+              </a>
+          </div>
+          <div className="footer-section right">
+            <div className="icon-container">
+              <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </div>
+            <div className="icon-container">
+              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </div>
+            <div className="icon-container">
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faXTwitter} />
+              </a>
+            </div>
+          </div>
       </footer>
-    );
-  }  
+  );
+}
 
 export default Footer;
